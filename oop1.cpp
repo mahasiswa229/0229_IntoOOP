268cfb4aecb872c2a5a69e22a30490fc62c9a0a9
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-04-23 16:01:00 +0800

    memanggil mahasiswa dan matkul

--- oop1.cpp
+++ oop1.cpp
@@ -44,5 +44,13 @@ int main()
     Mahasiswa mhs;
     Matakuliah mk;
 
-    
+    mhs.nama = "Budi";
+    mhs.umur = 20;
+    mhs.jurusan = "Teknik Informatika";
+    mhs.output();
+
+    mk.input();
+    mk.output();
+
+    return 0;
 }
