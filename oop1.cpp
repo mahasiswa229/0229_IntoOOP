cec8ccf3f5763fabc2471c1d25b22e17cdcc8ff6
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-04-23 15:56:33 +0800

    mengisi member output

--- oop1.cpp
+++ oop1.cpp
@@ -33,7 +33,9 @@ class Matakuliah
     }
     void output()
     {
-        
+        cout << "Kode MK : " << kodeMK << endl;
+        cout << "Nama MK : " << namaMK << endl;
+        cout << "SKS : " << sks << endl;
     }
 };
 
